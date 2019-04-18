f5631125e325dae2075f156ac8b3f85e770ce4ab
Alex Markessinis <markea125@gmail.com>
2019-04-17 20:23:07 -0400

    changed while loop to load changes to the config on the fly. accepts env vars for docker

--- piProbe.py
+++ piProbe.py
@@ -6,7 +6,7 @@ import time
 import Adafruit_DHT
 from influxdb import InfluxDBClient
 
-
+# Pull the configuratin from env vars or the config file
 def getConfig():
     if os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False):
         return {
@@ -36,67 +36,72 @@ def getConfig():
         print("Could not find configuration file.")
         exit(1)
 
+# The main program loop
+def mainLoop():
+    # get the config
+    config = getConfig()
+    # set the adafruit sensor
+    if config['gpio']['sensor'].upper() == 'DHT22':
+        sensor = Adafruit_DHT.DHT22
+    elif config['gpio']['sensor'].upper() == 'DHT11':
+        sensor = Adafruit_DHT.DHT11
+    elif config['gpio']['sensor'].upper() == 'AM2302':
+        sensor = Adafruit_DHT.AM2302
+    else:
+        print("The sensor entered is not supported.")
+        exit(2)
+    # Make a new influx client
+    client = InfluxDBClient(
+        host=config['influxdb']['host'],
+        port=int(config['influxdb']['port']),
+        username=config['influxdb']['user'],
+        password=config['influxdb']['password'],
+        database=config['influxdb']['dbname'],
+        ssl=bool(config['influxdb']['ssl']),
+        verify_ssl=bool(config['influxdb']['ssl_verify'])
+    )
+    # Poll the probe
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, int(config['gpio']['pin']))
+    # Don't accept null values, if they're null we don't sleep and we poll the probe again
+    if humidity is not None and temperature is not None:
+        # Filter stupid humidity readings, if the reading is high don't sleep and poll the probe again
+        if humidity <= 100:
+            # Format the measurements for influx
+            data = [
+                {
+                    "measurement": "temperature",
+                    "tags": {
+                        "host": socket.gethostname(),
+                        "location": config['influxdb']['location'],
+                    },
+                    "fields": {
+                        "value_c": float(temperature),
+                        "value_f": float(temperature * 9/5.0 + 32)
+                    }
+                },
+                {
+                    "measurement": "humidity",
+                    "tags": {
+                        "host": socket.gethostname(),
+                        "location": config['influxdb']['location'],
+                    },
+                    "fields": {
+                        "value": float(humidity)
+                    }
+                }
+            ]
+            # Write the data to influx
+            client.write_points(data, time_precision='s')
+            # wait it out
+            time.sleep(int(config['influxdb']['interval']))
+            # Destory the client
+            client.close()
+            client = None
+
+# Run it!
 try:
     while True:
-        # get the config
-        config = getConfig()
-        # set the adafruit sensor
-        if config['gpio']['sensor'].upper() == 'DHT22':
-            sensor = Adafruit_DHT.DHT22
-        elif config['gpio']['sensor'].upper() == 'DHT11':
-            sensor = Adafruit_DHT.DHT11
-        elif config['gpio']['sensor'].upper() == 'AM2302':
-            sensor = Adafruit_DHT.AM2302
-        else:
-            print("The sensor entered is not supported.")
-            exit(2)
-        # Make a new influx client
-        client = InfluxDBClient(
-            host=config['influxdb']['host'],
-            port=int(config['influxdb']['port']),
-            username=config['influxdb']['user'],
-            password=config['influxdb']['password'],
-            database=config['influxdb']['dbname'],
-            ssl=bool(config['influxdb']['ssl']),
-            verify_ssl=bool(config['influxdb']['ssl_verify'])
-        )
-        # Poll the probe
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, int(config['gpio']['pin']))
-        # Don't accept null values, if they're null we don't sleep and we poll the probe again
-        if humidity is not None and temperature is not None:
-            # Filter stupid humidity readings, if the reading is high don't sleep and poll the probe again
-            if humidity <= 100:
-                # Format the measurements for influx
-                data = [
-                    {
-                        "measurement": "temperature",
-                        "tags": {
-                            "host": socket.gethostname(),
-                            "location": config['influxdb']['location'],
-                        },
-                        "fields": {
-                            "value_c": float(temperature),
-                            "value_f": float(temperature * 9/5.0 + 32)
-                        }
-                    },
-                    {
-                        "measurement": "humidity",
-                        "tags": {
-                            "host": socket.gethostname(),
-                            "location": config['influxdb']['location'],
-                        },
-                        "fields": {
-                            "value": float(humidity)
-                        }
-                    }
-                ]
-                # Write the data to influx
-                client.write_points(data, time_precision='s')
-                # wait it out
-                time.sleep(int(config['influxdb']['interval']))
-                # Destory the client
-                client.close()
-                client = None
-
+        # Run the main loop
+        mainLoop()
 except KeyboardInterrupt:
     pass
